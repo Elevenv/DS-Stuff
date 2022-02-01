194dbf19a733e6e958720c69ae31cf33b2a08589
Elevenv <vitthaljadhav1420@gmail.com>
2022-02-01 12:11:06 +0530

    string can be travesed

--- bfs.py
+++ bfs.py
@@ -1,27 +1,44 @@
 import collections
 
 def bfs(graph,root):
-    visited = set()
+    visited = []
     queue = collections.deque([root])
-    visited.add(root)
+    print(queue)
+    # visited.add(root)
     while queue:
-        vertex = queue.pop()
-        visited.add(vertex)
+        vertex = queue.popleft()
+        if vertex not in visited:
+            visited.append(vertex)
+        print(vertex)
         for i in graph[vertex]:
             if i not in visited:
                 queue.append(i)
+                print(graph[vertex],'series : ',i)
+    # visited_set = set(visited)
     print(visited)
 
+# graph = {
+#     0:[1,2,3],
+#     1:[0,2],
+#     2:[0,1,4],
+#     3:[0],
+#     4:[2]
+# }
+
 graph = {
-    0:[1,2,3],
-    1:[0,2],
-    2:[0,1,4],
-    3:[0],
-    4:[2]
+    'a':['b','c'],
+    'b':['a','d','e'],
+    'c':['a','f','g'],
+    'd':[],
+    'e':[],
+    'f':[],
+    'g':[]
 }
+
 print(graph)
 print('BFS of above graph is : ')
-bfs(graph,0)
+# bfs(graph,0)
+bfs(graph,'a')
 
 
 
@@ -35,13 +52,3 @@ bfs(graph,0)
 
 
 
-
-# graph = {
-#     'a':['b','c'],
-#     'b':['a','d','e'],
-#     'c':['a','f','g'],
-#     'd':[],
-#     'e':[],
-#     'f':[],
-#     'g':[]
-# }
